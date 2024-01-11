28b94e8e30fe99161349ed0a3247c3e5ceb259a1
Ken-ser <karimbachir2001@gmail.com>
2024-01-11 15:07:26 +0100

    fix responseTime & better output

--- packages/main/src/app.ts
+++ packages/main/src/app.ts
@@ -13,18 +13,23 @@ const setupClient = (client: Client) => {
                 direction: "asc"
             },
             filter: {
-                title: "bers"
+                title: "be",
+                categories: ["8d9fbd22-9d54-410d-bb49-581f93efdf23"]
             }
         }
     ));
+    client.setHeaders({
+        "content-type": 'application/json'
+    })
+    client.on('response', handleResponse)
 };
 
 const test = autocannon({
     title: 'Facciamo scoppiare la Zipaki search: ',
     url: `${hostname}${path}${zipakiQueryParams}`,
     method: 'POST',
-    connections: 10,            // concurrent connections. OPTIONAL default: 10
-    amount: 10,                 // number of requests to make before ending the test
+    connections: 20,            // concurrent connections. OPTIONAL default: 10
+    amount: 20,                 // number of requests to make before ending the test
     //connectionRate: 2,        // rate of requests from each individual connection. OPTIONAL
     //overallRate: 2,           // rate of requests from all connections. OPTIONAL
     //timeout: 10,              // The number of seconds to wait for a response. OPTIONAL default: 10
@@ -34,18 +39,24 @@ const test = autocannon({
         console.log('error', err);
     handleResults(result);
 })
-// results passed to the callback are the same as those emitted from the done events
-test.on('tick', () => console.log('ticking'))
 
-test.on('response', handleResponse)
-
-function handleResponse(_client: Client, statusCode: number, responseTime: number) {
-    res.push(`Code ${statusCode} in ${responseTime} ms`)
+function handleResponse(statusCode: number, _resBytes: number, responseTime: number) {
+    res.push(`Code ${statusCode} in ${responseTime.toFixed()} ms`)
 }
 
 function handleResults(result: Result) {
     console.log('Replies', res);
-    console.log('Result', result);
+    console.log('Result', {
+        title: result.title,
+        url: result.url,
+        connections: result.connections,
+        pipelining: result.pipelining,
+        duration: result.duration,
+        errors: result.errors,
+        timeouts: result.timeouts,
+        resets: result.resets,
+        statusCodeStats: result.statusCodeStats
+    });
 }
 
 autocannon.track(test, { renderLatencyTable: true });
